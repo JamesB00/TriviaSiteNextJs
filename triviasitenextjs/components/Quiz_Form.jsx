77725b77b27efe9c63375e54f9afc8e040f7bd0d
unknown <jamesboynton00@gmail.com>
2023-07-22 17:17:51 -0600

    Quiz creation prototype implemented

--- triviasitenextjs/components/Quiz_Form.jsx
+++ triviasitenextjs/components/Quiz_Form.jsx
@@ -3,6 +3,28 @@ import { useState, useEffect } from "react";
 import Quiz_Form_Card from "@components/Quiz_Form_Card";
 
 const Quiz_Form = ({ data }) => {
+  const [quizData, setQuizData] = useState([]);
+
+  const getData = () => {
+    //Need to put all of these in their own loop to do it by question
+    //also need things for title, tag, etc.
+    const num = 0;
+    const arr = [];
+    const selAns = document.querySelector(
+      `input[name=quesAns_${num}]:checked`
+    ).value;
+    const corrAns = document.querySelector(
+      `input[name=ques_${num}_text_a${selAns}]`
+    ).value;
+    for (let i = 1; i < 5; i++) {
+      arr.push(
+        document.querySelector(`input[name=ques_${num}_text_a${i}]`).value
+      );
+    }
+    console.log(corrAns);
+    console.log(arr);
+  };
+
   return (
     <div className="flex flex-col justify-center">
       <div className="flex mx-auto">
@@ -18,7 +40,9 @@ const Quiz_Form = ({ data }) => {
         </label>
         <div className="bg-white rounded-r-lg w-5"></div>
       </div>
-      <Quiz_Form_Card data={null}></Quiz_Form_Card>
+
+      <Quiz_Form_Card data={null} qNum={0}></Quiz_Form_Card>
+      <button onClick={getData}>Click this one fr</button>
     </div>
   );
 };
--- triviasitenextjs/components/Quiz_Form_Card.jsx
+++ triviasitenextjs/components/Quiz_Form_Card.jsx
@@ -1,14 +1,19 @@
 "use client";
 import { useState, useEffect } from "react";
 
-const Quiz_Form_Card = ({ data }) => {
+//Need a way to match the value of the radio input to the text input for
+//the answers, somehow
+
+const Quiz_Form_Card = ({ data, qNum }) => {
+  const aNums = [1, 2, 3, 4];
   return (
-    <div className="flex flex-col my-2 mx-6 md:mx-16 lg:mx-24 border-2 border-blue-400 rounded-lg bg-white">
+    <div className="text-black flex flex-col my-2 mx-6 md:mx-16 lg:mx-24 border-2 border-blue-400 rounded-lg bg-white min-w-min">
       {/* Quiz Question */}
       <div className=" flex bg-white rounded-t-lg border-b-2 border-blue-400">
         <div className="w-full "></div>
         <label className="">
           <input
+            name={"ques_" + qNum}
             className="my-2 text-center"
             placeholder="Enter the question"
           ></input>
@@ -17,18 +22,21 @@ const Quiz_Form_Card = ({ data }) => {
       </div>
 
       {/* Quiz Answers */}
+
       <div className="sm:flex sm:flex-col justify-center bg-white  rounded-b-lg">
         {/* Using the empty divs with w-full to fill out input fields*/}
+
         <div className="flex my-1">
           <div className="w-full"></div>
           <label>
             <input
+              name={"ques_" + qNum + "_text_a1"}
               className="  text-center"
               placeholder="Enter answer choice 1"
             ></input>
           </label>
           <div className="w-full justify-center lg:justify-end lg:mr-4  flex">
-            <input name="ans_ques_0" type="radio"></input>
+            <input type="radio" name={"quesAns_" + qNum} value="1"></input>
           </div>
         </div>
 
@@ -36,12 +44,13 @@ const Quiz_Form_Card = ({ data }) => {
           <div className="w-full"></div>
           <label>
             <input
+              name={"ques_" + qNum + "_text_a2"}
               className="  text-center"
               placeholder="Enter answer choice 1"
             ></input>
           </label>
           <div className="w-full justify-center lg:justify-end lg:mr-4 flex">
-            <input name="ans_ques_0" type="radio"></input>
+            <input name={"quesAns_" + qNum} type="radio" value="2"></input>
           </div>
         </div>
 
@@ -49,12 +58,13 @@ const Quiz_Form_Card = ({ data }) => {
           <div className="w-full"></div>
           <label>
             <input
+              name={"ques_" + qNum + "_text_a3"}
               className="  text-center"
               placeholder="Enter answer choice 1"
             ></input>
           </label>
           <div className="w-full justify-center lg:justify-end lg:mr-4  flex">
-            <input name="ans_ques_0" type="radio"></input>
+            <input name={"quesAns_" + qNum} type="radio" value="3"></input>
           </div>
         </div>
 
@@ -62,16 +72,18 @@ const Quiz_Form_Card = ({ data }) => {
           <div className="w-full"></div>
           <label>
             <input
+              name={"ques_" + qNum + "_text_a4"}
               className="  text-center"
               placeholder="Enter answer choice 1"
             ></input>
           </label>
           <div className="w-full justify-center lg:justify-end lg:mr-4  flex">
-            <input name="ans_ques_0" type="radio"></input>
+            <input name={"quesAns_" + qNum} type="radio" value="4"></input>
           </div>
         </div>
 
         {/* Tag input */}
+        <button onClick={() => console.log(question)}>Click this one</button>
       </div>
     </div>
   );
