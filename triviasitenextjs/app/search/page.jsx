6bf095798bdcda012505254edff4a3578e8e80cc
jamesboynton00 <jamesboynton00@gmail.com>
2023-08-11 19:31:22 -0600

    Updates to search page, now maps results to cards

--- triviasitenextjs/app/search/page.jsx
+++ triviasitenextjs/app/search/page.jsx
@@ -1,35 +1,73 @@
 "use client";
 import { Container } from "postcss";
 import { useState, useEffect } from "react";
-import Quiz_Card from "@components/Quiz_Card";
+import Quiz_Search_Card from "@components/Quiz_Search_Card";
 
 //Not technically sure if this dynamic routing is correct, but it seems to be working fine
 
+const SearchQuizCards = ({ data, search, searchStatus }) => {
+  if (search == "") {
+    return (
+      <div className="flex justify-center">
+        <p>Type in a tag to find your next quiz!</p>
+      </div>
+    );
+  }
+  if (data.quizzes == undefined) {
+    return;
+  }
+  if (Object.values(data.quizzes).length == 0) {
+    return (
+      <div className="flex justify-center">
+        <p>No results found for "{search}"</p>
+      </div>
+    );
+  } else {
+    return Object.values(data.quizzes).map((elem, index) => (
+      <Quiz_Search_Card
+        quizTitle={elem.title}
+        quizCreator={elem.creator}
+        quizTag={elem.tag}
+        key={"searchCard_" + index}
+      ></Quiz_Search_Card>
+    ));
+  }
+};
+
 const search = () => {
   const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [toggleSearching, setToggleSearching] = useState(false);
 
   const handleSearchTextChange = (e) => {
     setSearchText(e.target.value);
   };
 
   const getQuizzes = async (searchTxt) => {
+    if (searchTxt == "") {
+      return;
+    }
+    setToggleSearching((prev) => !prev);
     const res = await fetch(`/api/searchQuizzes/${searchTxt}`, {
       method: "GET",
     });
     const data = await res.json();
 
-    console.log(data);
+    setSearchResults(data);
+    setToggleSearching((prev) => !prev);
   };
 
   useEffect(() => {
-    const timeOutId = setTimeout(() => getQuizzes(searchText), 1000);
+    //Use a timout to not constantly be pulling from database on every character change
+    const timeOutId = setTimeout(() => {
+      getQuizzes(searchText);
+    }, 1000);
     return () => clearTimeout(timeOutId);
-    //Here, I'll need to make an api request to get quizzes back to populate the page with
-    //by way of setting the quizResults state
   }, [searchText]);
 
   return (
     <section className="my-2 mx-16 lg:mx-32">
+      {/* Search Input Bar */}
       <div className="flex text-black">
         <div className="w-2 bg-white rounded-l-lg" />
         <textarea
@@ -40,27 +78,20 @@ const search = () => {
         <div className="w-2 bg-white rounded-r-lg" />
       </div>
       <br></br>
+
+      {/* Search Results */}
       <div className="bg-white rounded-md text-black flex flex-col justify-center">
-        <Quiz_Card
-          quizTitle={"First Title"}
-          quizCreator={"me@gmail.com"}
-          quizTag={"Misc"}
-        ></Quiz_Card>
-        <Quiz_Card
-          quizTitle={"Title"}
-          quizCreator={"me@gmail.com"}
-          quizTag={"Misc"}
-        ></Quiz_Card>
-        <Quiz_Card
-          quizTitle={"Title"}
-          quizCreator={"me@gmail.com"}
-          quizTag={"Misc"}
-        ></Quiz_Card>
+        <SearchQuizCards
+          data={searchResults ? searchResults : null}
+          search={searchText}
+          status={toggleSearching}
+        />
       </div>
+      <br />
 
       {/* Button to load more results */}
       <div className="flex justify-center">
-        <button className="bg-blue-600 hover:bg-blue-400 rounded-lg p-1 m-1">
+        <button className="bg-blue-600 hover:bg-blue-400 rounded-lg p-1">
           Load More
         </button>
       </div>
